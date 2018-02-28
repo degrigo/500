a4ea17bbe11d585427e3652659a8646ac67ab47d
Tadeu Grigolon <degrigo@gmail.com>
2018-02-28 19:16:12 -0300

    Added op-ternario

---
+++ aula03/op-ternario.php
@@ -0,0 +1,17 @@
+<?php
+
+$a = 1;
+$b = 0;
+
+// if ($a > $b) {
+// 	echo "A é maior que B";
+// } else {
+// 	"A é menor que B";
+// }
+
+echo "<hr>";
+
+echo ($a > $b) ? "A é maior que B" : "A é menor que B";
+
+// Seto condição e pergunto esperando a resposta para a verificação das variáveis.
+// É uma forma reduzida do if.
