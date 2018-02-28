afea35466f084f20990e4b84bb157ce0d95852fc
Tadeu Grigolon <degrigo@gmail.com>
2018-02-28 20:03:45 -0300

    Added for

---
+++ aula03/for.php
@@ -0,0 +1,13 @@
+<?php
+
+// Faz a mesma coisa que o While, mas de maneira mais simplicada
+// dentro do for: valor, condição e incremento/decremento
+
+echo "<pre>";
+for ($cont = 0; $cont <= 10; $cont ++) { 
+	echo $cont . "\n";
+}
+
+for ($cont = 10; $cont >= 0; $cont --) { 
+	echo $cont . "\n";
+}
