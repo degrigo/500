91fcc3d5d6526d8c748ae85be6b723f55419bd0a
Tadeu Grigolon <degrigo@gmail.com>
2018-03-01 19:45:58 -0300

    Added array-associativo

--- aula04/array-associativo.php
+++ aula04/array-associativo.php
@@ -1,14 +1,26 @@
 <?php
 
+// Array associativo cria um nome para o índice, conseguindo imprimir o valor através do mesmo
+// print_r mostra o conteúdo do array
+// var_dump mostra o conteúdo e a tipagem
+
 echo "<pre>";
 
 echo "<hr>";
 
-$pessoa = [
+$conta = 4;
 
+$pessoa = [
 	"nome" => "Tadeu Grigolon",
 	"idade" => 34,
-	"altura" => 1.78
+	"altura" => 1.78,
+	"time" => "Palmeiras",
+	"cidade" => "São Bernardo do Campo"
 ];
 
 print_r($pessoa);
+var_dump($pessoa);
+
+echo "<hr>";
+
+echo $pessoa['time'];
