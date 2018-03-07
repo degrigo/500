f21ee20c6b4ddd073a09180378d00767032b0beb
Tadeu Grigolon <degrigo@gmail.com>
2018-03-06 22:24:20 -0300

    developed mysqli-query

--- aula07/mysql.php
+++ aula07/mysql.php
@@ -9,4 +9,9 @@ $db	  = "tadeu";
 
 $conection = mysqli_connect($host, $user, $pass, $db);
 
+// Inserindo dados no banco utilizando mysqli_query(conexão, query sql)
+
+mysqli_query($conection, "INSERT INTO posts (autor, post) VALUES (2, 'Meu quarto post');");
+mysqli_query($conection, "INSERT INTO posts (autor, post) VALUES (3, 'Meu quarto post');");
+
 var_dump($conection);
